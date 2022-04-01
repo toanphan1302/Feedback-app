09f29c968177dee33971ec6e9dd24022e1e7d7c8
Toan Phan <toanphan1302@gmail.com>
2022-04-01 09:43:18 -0700

    form3

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,43 @@
+import {useState} from 'react'
+import Card from './shared/Card'
+import Button from './shared/Button';
+
+function FeedbackForm() {
+    const [text, setText] = useState('');
+    const [btnDisable, setBtnDisable] = useState(true);
+    const [message, setMessage] = useState('');
+
+    const handleTextChange = (e) => {
+        if(text ==='') {
+            setMessage(null)
+            setBtnDisable(true)
+        } else if (text !== '' && text.trim().length <= 10){
+            setMessage('Text must be at least 10 characters')
+            setBtnDisable(true)
+        }
+        else{
+            setMessage(null)
+            setBtnDisable(false)
+        }
+
+        setText(e.target.value)
+    }
+
+  return (
+    <Card>
+      <h2>How would you rate your service with us?</h2>
+      <div className='input-group'>
+          <input onChange={handleTextChange}
+                 type='text'
+                 placeholder='Write a review'
+                 value={text}
+                 />
+          <Button type="submit" isDisable={btnDisable}>Send</Button>
+      </div>
+
+      {message && <div className='message'>{message}</div>}
+    </Card>
+  )
+}
+
+export default FeedbackForm
---
+++ src/components/shared/Button.jsx
@@ -0,0 +1,15 @@
+function Button({children, version, type, isDisable}) {
+  return (
+    <button type={type} disabled={isDisable} className={`btn btn-${version}`}> 
+      {children}
+    </button>
+  )
+}
+
+Button.defaultProps = {
+    version: 'primary',
+    type: 'button',
+    isDisable: false
+}
+
+export default Button
