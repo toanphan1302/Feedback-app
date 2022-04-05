e73522e7205ab984c850adfae33f697dd68c59d5
Toan Phan <toanphan1302@gmail.com>
2022-04-04 22:48:15 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { v4 as uuidv4 } from "uuid";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
+import AboutIconlink from "./components/AboutIconlink";
+import { Link } from "react-router-dom";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -21,14 +25,29 @@ function App() {
   };
 
   return (
-    <>
+    <Router>
       <Header />
-      <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+      <div exact className="container">
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+        <AboutIconlink />
       </div>
-    </>
+    </Router>
   );
 }
 
